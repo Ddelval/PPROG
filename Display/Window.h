d1e76490cd653ef4a340fbbf47412b3eebf373cc
anton-seoane <37266640+anton-seoane@users.noreply.github.com>
2019-09-28 15:08:18 +0100

    Update Window.h

--- Display/Window.h
+++ Display/Window.h
@@ -6,12 +6,12 @@
 
 typedef struct _Window Window;
 
-Window* win_ini(char* title, Welem** Win_elem, int wid, int hei);
+Window* win_ini(char* title, Welem** Win_elem, int num_elems, int wid, int hei, int weight);
 void win_free(Window* win);
-Window* win_redraw(Window* win, int wid, int hei);
+Window* win_redraw(Window* win, int wid, int hei, int weight);
 
 Window* win_setSelected(Window* win, int* selected_elem);
-Welem* win_getSelected(Window* win);
+Welem** win_getSelected(Window* win);
 
 Window* win_scrollDown(Window* win);
 Window* win_scrollUp(Window* win);
