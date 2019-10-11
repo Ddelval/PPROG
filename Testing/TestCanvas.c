3600ec5639be10ac0fd7deafa52a3fa368adab91
Antón Seoane <anton.seoane@outlook.com>
2019-10-11 09:12:48 +0200

    Valgrind minor

--- Testing/TestCanvas.c
+++ Testing/TestCanvas.c
@@ -9,10 +9,13 @@
 #include "Canvas.h"
 
 int main(int argc, const char * argv[]) {
-  FILE* f = fopen("dat.txt", "r");
-	Canvas* can=canv_load(f);
-	Canvas * c=canv_backGrnd(255, 255, 255, 255, canv_getWidth(can), canv_getHeight(can));
-	canv_addOverlay(c, can, 0, 0);
-	canv_print(stdout, c, 0, 0);
+    FILE* f = fopen("dat.txt", "r");
+    Canvas* can=canv_load(f);
+    Canvas* c=canv_backGrnd(255, 255, 255, 255, canv_getWidth(can), canv_getHeight(can));
+    canv_addOverlay(c, can, 0, 0);
+    canv_print(stdout, c, 0, 0);
+    canv_free(can);
+    canv_free(c);
+    fclose(f);
   return 0;
 }
