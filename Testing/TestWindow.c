05fc98ff49d117b9ae2484d853e497f6639116f6
David del Val Moncholi <delvaldavid1@gmail.com>
2019-10-24 22:03:20 +0200

    fix

--- Testing/TestWindow.c
+++ Testing/TestWindow.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <errno.h>
 #include "Window.h"
-
+#include "Welem.h"
+#include "Wlabel.h"
 int main(int argc, const char* argv[]) {
 	Window* win = win_ini("Inventory", NULL, 0, 300, 250, 1, 909, 0);
 	if(!win) {
@@ -9,12 +10,21 @@ int main(int argc, const char* argv[]) {
 		return 1;
 	}
 	errno = 0;
+	FILE* fi=fopen("Display/Fonts/Robo_Mono/04.txt", "r");
+	Font* f=font_load(fi);
+	Wlabel* w=wl_ini("5x data",f,10);
+	Welem* wel=we_ini(LABEL,w);
+	win_addWindowElement(win,wel);
 	if(!win_setMargins(win, 0, 0, 0, 0)) printf("aaa");
 	if(!win_render(win)) {
 		fprintf(stderr, "BAD RENDER");
 		win_free(win);
 		return 2;
 	}
+	font_free(f);
+	fclose(fi);
+	wl_free(w);
+	we_free(wel);
 	win_free(win);
 	return 0;
 }
