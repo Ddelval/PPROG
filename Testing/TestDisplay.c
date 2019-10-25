a79ccf360ec83ee3047cc8400a4ec261e8af25bb
Anton Seoane Ampudia <anton.seoane@outlook.com>
2019-10-25 20:48:50 +0200

    asdf

--- Testing/TestDisplay.c
+++ Testing/TestDisplay.c
@@ -7,5 +7,12 @@
 #include "Room.h"
 
 int main(int argc, const char* argv[]) {
-  
+  Canvas* c=canv_backGrnd(0,255,0,255,700,470);
+  FILE* f=fopen("Display/Fonts/Robo_Mono/06.txt", "r");
+  Font* fu;
+  Font* fo=font_load(f);
+  fclose(f);
+  f=fopen("Display/Fonts/Robo_Mono/06.txt", "r");
+  Welem* we=we_createLabel("Resources", f, 10);
+ // atom://teletype/portal/17a338be-4b30-4988-a436-2bd408be4641
 }
