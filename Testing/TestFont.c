0231d7ef6917b5809cb2548d690954da88d04e84
Anton Seoane Ampudia <anton.seoane@outlook.com>
2019-10-25 20:25:05 +0200

    changes

--- Testing/TestFont.c
+++ Testing/TestFont.c
@@ -8,17 +8,16 @@
 #include "Font.h"
 #include "Canvas.h"
 int main(int argc, const char * argv[]) {
-    FILE* f = fopen("Display/Fonts/Robo_Mono/06.txt", "r");
+    FILE* f = fopen("Display/Fonts/Robo_Mono/04.txt", "r");
     Font *ff=font_load(f);
     Canvas* c=font_renderText(ff, "Hello, world");
     Canvas* back=canv_backGrnd(0, 0, 255, 255, canv_getWidth(c), canv_getHeight(c));
     canv_addOverlay(back, c, 0, 0);
     canv_print(stdout, back, 10, 10);
-    f = fopen("Display/Fonts/Robo_Mono/04.txt", "r");
-    ff=font_load(f);
-    c=font_renderText(ff, "Hello, world");
-    back=canv_backGrnd(0, 0, 255, 255, canv_getWidth(c), canv_getHeight(c));
-    canv_addOverlay(back, c, 0, 0);
-    canv_print(stdout, back, 50, 10);
+    fclose(f);
+    font_free(ff);
+    canv_free(c);
+    canv_free(back);
+
     return 0;
 }
