36af50e6cd693989627c41d57dba2a6530c8fc3a
Anton Seoane Ampudia <anton.seoane@outlook.com>
2019-10-25 20:35:30 +0200

    Commit message

---
+++ Testing/TestDisplay.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <errno.h>
+#include "Window.h"
+#include "Welem.h"
+#include "Wlabel.h"
+#include "Display.h"
+#incluse "Room.h"
+
+int main(int argc, const char* argv[]) {
+
+}
