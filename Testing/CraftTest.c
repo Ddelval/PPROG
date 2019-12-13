e5027881f67756b03c781524ea1e645821d8e752
pedrou2000 <46072805+pedrou2000@users.noreply.github.com>
2019-12-13 20:04:52 +0100

    Updated CraftTest

--- Testing/CraftTest.c
+++ Testing/CraftTest.c
@@ -6,40 +6,17 @@
 #include "Crafting.h"
 
 int main(){
-  Recipe *r = rec_ini();
-  if(!r){
-    printf("!r\n");
-    exit(0);
-  }
+  Recipe *r ;
+  int size ;
+  int *q, *e, i = 0;
 
   FILE * f = fopen("Dictionaries/craft_recipies.txt", "r");
   if(!f){
     printf("!r 2\n");
     exit(0);
   }
-  r = rec_load(f);
-  if(!r){
-    printf("!r 3\n");
-    exit(0);
-  }
 
-
-  int size = rec_getSize(r);
-  printf("Result Id: %d, Size: %d, Name: %s\n", rec_getResult_id(r), size, rec_getName(r));
-  int *q, *e;
-  q = rec_getQuantities(r);
-  e = rec_getElements(r);
-  for(int i= 0; i<size; i++){
-    printf("Object id: %d, Quantiy: %d\n", q[i], e[i]);
-  }
-
-  rec_free(r);
-
-
-
-
-
-
+while(i < 4){
   r = rec_ini();
   if(!r){
     printf("!r\n");
@@ -60,10 +37,14 @@ int main(){
   for(int i= 0; i<size; i++){
     printf("Object id: %d, Quantiy: %d\n", q[i], e[i]);
   }
+  q = NULL;
+  e = NULL;
 
   rec_free(r);
-  fclose(f);
+  i++;
+}
 
+  fclose(f);
 
 
   return 1;
