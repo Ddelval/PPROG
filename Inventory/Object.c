d4c4006aa4e43ee32c5450bfbbdec8125e5ed66d
Antón Seoane <anton.seoane@outlook.com>
2019-12-22 20:38:05 +0100

    Commit

--- Inventory/Object.c
+++ Inventory/Object.c
@@ -82,12 +82,12 @@ Object* obj_load(FILE* f){
 
     }
     if(ob->type==CONSUMABLE){
-        //ob->atb=atb_load(f);
+        ob->atb=attb_load(f);
     }
     if(ob->type==WEAPON){
         fscanf(f,"%d",&ob->n_attacks);
         for(int i=0;i<ob->n_attacks;++i){
-            //ob->attacks[i]=skill_load(f);
+            ob->attacks[i]=skill_load(f);
         }
     }
     fscanf(f,"%d",(int*)(&ob->destroyable));
