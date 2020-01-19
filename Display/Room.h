2e18d610f2b4844d3edf187e9e46bfed622acc27
Antón Seoane <anton.seoane@outlook.com>
2020-01-19 17:43:11 +0100

    Now the building window doesn't have an uncleared display right

--- Display/Room.h
+++ Display/Room.h
@@ -20,7 +20,7 @@ typedef struct _Room Room;
 /*-----------------------------------------------------------------*/
 /**
  * @brief Allocates all the memory required for the new room
- * 
+ *
  * @param id      Id of the room
  * @param name    Name of the room
  * @param hei     Total height of the room
@@ -33,7 +33,7 @@ Room* room_ini(int id, const char* name,int hei, int wid, const Pixel* backcol);
 /*-----------------------------------------------------------------*/
 /**
  * @brief Frees the memory allocated by the room
- * 
+ *
  * @param r Room to be freed
  */
 void room_free(Room* r);
@@ -41,16 +41,16 @@ void room_free(Room* r);
 /*-----------------------------------------------------------------*/
 /**
  * @brief Loads a room from a file
- * 
+ *
  * The format of the room should be:
  * <id> <length of Name> <name>
  * <height> <width>
  * <background color> (as a Pixel)
  * <number of background sprites>
- * 
+ *
  * For each sprite:
  * <id> <ipos> <jpos>
- * 
+ *
  * @param f File from where the room will be read
  * @return  New room containing all the data
  */
@@ -60,7 +60,7 @@ Room* room_load(FILE* f);
 /*-----------------------------------------------------------------*/
 /**
  * @brief Copies the room
- * 
+ *
  * @param r     Room to be copied
  * @return      Identical copy of r
  */
@@ -70,10 +70,10 @@ Room* room_copy(const Room* r);
 /*-----------------------------------------------------------------*/
 /**
  * @brief Adds a sprite to the background layer
- * 
+ *
  * This is the function that should be called when we want to add
  * a sprite that does not proceed from an entity
- * 
+ *
  * @param r Room in which the sprite will be added
  * @param s Sprite to be added
  * @return  NULL in case of error
@@ -83,11 +83,11 @@ Room* room_addBSprite(Room* r, const Sprite* s);
 /*-----------------------------------------------------------------*/
 /**
  * @brief Add a sprite to the top layer
- * 
+ *
  * This is the function that should be called to add sprites that
  * come from entities. In doing so, they will be able to interact
  * with the player in a different way (talk and combat)
- * 
+ *
  * @param r     Room in which the sprite will be added
  * @param s     Sprite to add
  * @param ent_t Type of the entity that is associated with the sprite
@@ -95,15 +95,15 @@ Room* room_addBSprite(Room* r, const Sprite* s);
  *              2 -> Enemy
  *              3 -> Ally
  * @return      -1 in case of error
- *              Otherwise, the index of the sprite in r->overs after 
- *              it has been added  
+ *              Otherwise, the index of the sprite in r->overs after
+ *              it has been added
  */
 int room_addOSprite(Room* r, const Sprite* s,int ent_t);
 
 /*-----------------------------------------------------------------*/
 /**
  * @brief Gets the position of a background sprite
- * 
+ *
  * @param r     Room in which the sprite is
  * @param index Position of the sprite in r->backg
  * @param i     Pointer to a variable where the i-coordinate will be
@@ -117,9 +117,9 @@ Room* room_getBSpritePos(const Room *r, int index, int* i, int *j);
 /*-----------------------------------------------------------------*/
 /**
  * @brief Renders the room
- * 
+ *
  * Note that this function will not redraw the base layer
- * 
+ *
  * @param r Room to be rendered
  * @return Graphical representation of the room
  */
@@ -128,18 +128,18 @@ Canvas* room_getRender(const Room* r);
 /*-----------------------------------------------------------------*/
 /**
  * @brief Modifies the position of a sprite of the top layer
- * 
- * Note that this function will change the position if possible 
+ *
+ * Note that this function will change the position if possible
  * and will renturn a value that represents the current position
  * of the sprite.
- * 
- * 
- * 
+ *
+ *
+ *
  * @param r      Room in which the sprite is
  * @param index  Index of the sprite in r->overs
  * @param i      New i-coordinate of the top left corner of the sprite
  * @param j      New j-coordinate of the top left corner of the sprite
- * @param scroll Whether or not the movement of this sprite should 
+ * @param scroll Whether or not the movement of this sprite should
  *               affect the scrolling of the room
  * @return       Integer code:
  *               -1: error
@@ -148,25 +148,25 @@ Canvas* room_getRender(const Room* r);
  *                2: the sprite is in contact with the right border
  *                3: the sprite is in contact with the bottom border
  *                4: the sprite is in contact with the left border
- *                5: the sprite cannot move because of a collision 
+ *                5: the sprite cannot move because of a collision
  */
 int room_modPos(Room* r, int index, int i, int j, bool scroll);
 
 /*-----------------------------------------------------------------*/
 /**
  * @brief Increments the position of a sprite of the top layer
- * 
- * Note that this function will change the position if possible 
+ *
+ * Note that this function will change the position if possible
  * and will renturn a value that represents the current position
  * of the sprite.
- * 
- * 
- * 
+ *
+ *
+ *
  * @param r      Room in which the sprite is
  * @param index  Index of the sprite in r->overs
  * @param i      Increment in the i axis
  * @param j      Increment in the j axis
- * @param scroll Whether or not the movement of this sprite should 
+ * @param scroll Whether or not the movement of this sprite should
  *               affect the scrolling of the room
  * @return       Integer code:
  *               -1: error
@@ -175,14 +175,14 @@ int room_modPos(Room* r, int index, int i, int j, bool scroll);
  *                2: the sprite is in contact with the right border
  *                3: the sprite is in contact with the bottom border
  *                4: the sprite is in contact with the left border
- *                5: the sprite cannot move because of a collision 
+ *                5: the sprite cannot move because of a collision
  */
 int room_incPos(Room* r, int index, int i, int j,bool scroll);
 
 /*-----------------------------------------------------------------*/
 /**
  * @brief Checks if a combat should be started
- * 
+ *
  * @param r     Room in which the entities are
  * @param index Index of the entity that has moved
  * @return      NULL if no combat should be started
@@ -192,42 +192,42 @@ int room_incPos(Room* r, int index, int i, int j,bool scroll);
 Trigger* room_checkCombat(Room* r,int index);
 /*-----------------------------------------------------------------*/
 /**
- * @brief Sets the are of the window that will be rendered when 
+ * @brief Sets the are of the window that will be rendered when
  *        the rendering functions are called
- * 
+ *
  * This is used when the player moves to another section of the map
  * or when the room is set up initially.
- * 
+ *
  * @param ro Room
  * @param t  Top limit,      included
- * @param l  Left limit,     included 
+ * @param l  Left limit,     included
  * @param b  Bottom limit,   excluded
  * @param r  Right limit,    excluded
- * @return   NULL if error, the given room otherwise 
+ * @return   NULL if error, the given room otherwise
  */
 Room* room_setBounds(Room*ro, int t, int l, int b, int r);
 
 /*-----------------------------------------------------------------*/
 /**
- * @brief Prints the modifications to the room, after the position 
+ * @brief Prints the modifications to the room, after the position
  *        of an entity has changed
- * 
+ *
  * @param r      Room to be updated
  * @param index  Index of the sprite in overs that has changed
  * @param disp_i Vertical offset of the room (generally 0)
  * @param disp_j Horizontal offset of the room (generally 0)
- * @return Room* 
+ * @return Room*
  */
 Room* room_printMod(Room* r, int index, int disp_i, int disp_j);
 
 /*-----------------------------------------------------------------*/
 /**
  * @brief Scrolls the window by a factor of (i,j)
- * 
+ *
  * Usually the factors are (-1 <= i <= 1)
- * 
+ *
  * @param r Room to be scrolled
- * @param i Horizontal factor 
+ * @param i Horizontal factor
  * @param j Vertical factor
  * @return  -1 if there was an error
  *           0 if there was no scrolling to be done
@@ -239,7 +239,7 @@ int room_scroll(Room* r, double i, double j);
 /*-----------------------------------------------------------------*/
 /**
  * @brief Adds the trigges of sp to the room
- * 
+ *
  * @param r     Room to which the triggers are added
  * @param sp    Sprite that contains the triggers
  * @param index Index of sp in the array overs
@@ -252,10 +252,10 @@ Room* room_processTriggers(Room * r, const Sprite * sp, int index);
 /**
  * @brief Triggers associated to the position of the sprite
  *        r->overs[sp_index]
- * 
+ *
  * The trigger will be returned if it is associated to the
  * position of any of the four corners of the sprite
- * 
+ *
  * @param r         Room that contains the sprite
  * @param tt        Type of trigger that we are searching for
  * @param sp_index  Index of the sprite in the array overs
@@ -269,7 +269,7 @@ Trigger** room_getTriggers(Room*r,tr_type tt, int sp_index, int* siz);
 /*-----------------------------------------------------------------*/
 /**
  * @brief Renders the map again
- * 
+ *
  * @param r Room to be recalculated
  * @return  NULL if error
  */
@@ -277,9 +277,9 @@ Canvas* room_redrawMap(Room*r);
 
 /*-----------------------------------------------------------------*/
 /**
- * @brief Updates all the data in room after the deletion of a 
+ * @brief Updates all the data in room after the deletion of a
  *        sprite
- * 
+ *
  * @param r Room to be updated
  * @return  NULL if error
  */
@@ -289,7 +289,7 @@ Room* room_updateData(Room*r);
 /**
  * @brief Prints the changes to the background after deletion of
  *        a sprite
- * 
+ *
  * @param r      Room to be updated
  * @param disp_i Horizontal offset of the screen
  * @param disp_j Vertical offset of the screen
@@ -300,7 +300,7 @@ Room* room_printModBackg(Room* r, int disp_i, int disp_j);
 /*-----------------------------------------------------------------*/
 /**
  * @brief Removes the sprite in position index from r->backg
- * 
+ *
  * @param r     Room to be updated
  * @param index Index of the sprite to be removed
  * @return      NULL if error
@@ -310,7 +310,7 @@ Room* room_removeB(Room* r, int index);
 /*-----------------------------------------------------------------*/
 /**
  * @brief Removes a sprite from the array overs
- * 
+ *
  * @param r     Room from which the sprite will be deleted
  * @param index Index of the sprite in the array overs
  * @return      NULL if error
@@ -320,22 +320,23 @@ Room* room_removeOver(Room* r, int index);
 /*-----------------------------------------------------------------*/
 /**
  * @brief Displays the building interface
- * 
+ *
  * @param r      Room in which the building will be placed
  * @param spid   Sprite id of the building
  * @param ai     Initial vertical coordinate of the sprite
  * @param aj     Initial horizontal coordinate of the sprite
  * @param room_i Horizontal offset of the room
  * @param room_j Vertical offset of the room
+ * @param dims   Dimensions of the parent display of the room
  * @return       NULL if error
  */
-Room* room_buildingInterface(Room*r, int spid,int ai, int aj,int room_i, int room_j);
+Room* room_buildingInterface(Room*r, int spid,int ai, int aj,int room_i, int room_j, int* dims);
 
 /*-----------------------------------------------------------------*/
 /**
  * @brief Modifies the room when an ally has been added
- * 
- * @param r            Room to be updated 
+ *
+ * @param r            Room to be updated
  * @param e            Entity that represents the ally
  * @param s            Sprite of the ally
  * @param ally_index   Index of the ally in the sprite array
@@ -347,19 +348,19 @@ Room* room_processAlly(Room* r, void *e,const Sprite* s,int ally_index, int rad)
 /*-----------------------------------------------------------------*/
 /**
  * @brief Adds an enemy to the room
- * 
- * Note that the sprite will already be in the overs array sice it 
+ *
+ * Note that the sprite will already be in the overs array sice it
  * is added to that one as soon as the entity is initalized.
- * 
+ *
  * @param r             Room in which the enemy will be inserted
  * @param e             Pointer to the entity
- * @param enemy_index   Index of the sprite of the enemy in the 
+ * @param enemy_index   Index of the sprite of the enemy in the
  *                      array overs
  * @param i1            Top limit of the attack area
  * @param i2            Bottom limit of the attack area
  * @param j1            Left limit of the attack area
  * @param j2            Right limit of the attack area
- * @return Room* 
+ * @return Room*
  */
 Room* room_processEnemy(Room* r,void* e,int enemy_index, int i1,int i2,int j1,int j2);
 
