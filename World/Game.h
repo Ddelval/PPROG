dfe16be21a665f9550145656b7b910c1db972ba5
pedrou2000 <46072805+pedrou2000@users.noreply.github.com>
2020-01-20 10:02:09 +0100

    Commented game.h

--- World/Game.h
+++ World/Game.h
@@ -5,7 +5,21 @@
 #include "World.h"
 
 typedef struct _Game Game;
-Game* game_loadWorlds();
 
+/**
+ * @brief Loads all the worlds
+ *
+ * This function will load all the worlds that
+ * are stored in the folder World/
+ *
+ * @param g Game in which the worlds will be loaded
+ * @return  NULL if error
+**/
+Game* game_loadWorlds();
+/**
+ * @brief Opens, initializes, loads the worlds and execute the game.
+ *        Finally closes and frees it.
+ * @return  1 in case of error 0 otherwise.
+**/
 int game_launch();
 #endif
