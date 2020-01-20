dde5c3a58992471746745554ff6939355563ac47
pedrou2000 <46072805+pedrou2000@users.noreply.github.com>
2020-01-20 11:30:27 +0100

    Some more comments on DialogMan.h

--- Entity/DialogMan.h
+++ Entity/DialogMan.h
@@ -39,11 +39,28 @@ Dialog* dman_lookup(DialogMan* dman, int did);
     Resets a given dialog to its beggining state.
 */
 DialogMan* dman_resetDialog(DialogMan* dd);
-
-
+/*
+    Returns the a char pointer containing the string of the next line of the
+    dialog passed as input.
+*/
 char* dman_getLine(DialogMan* dic);
+/*
+    Sets the current state of the dialog man to the dialog whose id is the
+    one passed as input.
+*/
 DialogMan* dman_setDialog(DialogMan* dman, int did);
+/*
+    Sets the dialog man to the next dialog in the conversation. If it is the
+    last dialog, returns NULL.
+*/
 DialogMan* dman_advance(DialogMan* dd);
+/*
+    Gets the quest of the cureent dialog in the DialogMan.
+*/
 Quest* dman_fetchQuest(DialogMan* dd);
+/*
+    Sets the dialog man to the next dialog in the conversation. Increments the
+    global variable tier.
+*/
 void dman_autoAdvance(DialogMan* dd);
 #endif /* DIALOGMAN_H */
