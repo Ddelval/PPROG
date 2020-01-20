db0d110c3d9e20408d9a8fecb4096d25a2915821
pedrou2000 <46072805+pedrou2000@users.noreply.github.com>
2020-01-20 11:57:24 +0100

    Some more commments on Utility.h

--- Utility/Utility.h
+++ Utility/Utility.h
@@ -29,18 +29,39 @@ typedef struct {
     int fi,se;
 }pairii;
 
+/*
+      Appends to the destination string the origin one, saving the total length
+      in spos.
+*/
 void appendf(char* dest, int* spos,char* orig);
 void append (char* dest, int* spos, const char* orig);
+/*
+      Writes in a char the position of the cursor.
+*/
 char* movecur(int x,int y);
 int max(int a,int b);
 int min(int a,int b);
-
+/*
+      Reads an input written on  stdin.
+*/
 char getch1(void);
+/*
+      Reads an input written on stdin till a char Q, E or J is read.
+*/
 char waitforchar(void);
 void term_restore();
 void term_init();
+/*
+      Writes on the given file a string.
+*/
 int sendToScreen(FILE* f, char* ch);
 
+/*
+      Used to check that the construction of a building has been succesful.
+*/
 bool checkBuilding(pairii p);
+/*
+      Used to construct a building
+*/
 void addBuilding(pairii p);
 #endif /* Utility_h */
